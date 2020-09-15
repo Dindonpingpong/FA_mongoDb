1ee74c2240f20e0245661a95d408e1d3150bd52e
Rubi Kina <rkina@dindon.com>
2020-09-15 21:58:55 +0300

    half of lab 1

--- test.js
+++ test.js
@@ -2,9 +2,16 @@
 let MongoClient = require('mongodb').MongoClient;
 let url = "mongodb://localhost:27017";
 
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('Students.json');
+let student = JSON.parse(rawdata);
+console.log(student);
+
+
 MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     console.log("Database created!");
-    
+
     db.close();
 });
